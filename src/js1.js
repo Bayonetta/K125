72bffe0f20dc56d246c140b652dd48529ee88a3e
Bayonetta_zs <shuaizhao0315@outlook.com>
2024-04-27 18:23:20 +0900

    update

---
+++ src/js1.js
@@ -0,0 +1,23 @@
+let product = 0;
+for (let i = 1; i <= 5; i++) {
+    product *= i;
+}
+
+let sum = 0;
+for (let i = 1; i <= 5; i++) {
+    sum += i;
+}
+
+let quotient = product / sum;
+
+
+console.log(sum);
+
+
+let a1 = 2;
+let a2 = 3;
+let a3 = 4;
+
+let average = (a1 + a2 + a3) / 3;
+
+console.log(average);
